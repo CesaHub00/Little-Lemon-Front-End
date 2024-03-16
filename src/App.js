846ddeb39bf30978868281e42c09c1b44cd77f4d
CesaHub00 <cesarenava247@gmail.com>
2024-03-16 17:07:41 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import './App.css';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Nav from './components/Nav';
-import Main from './components/Main';
+import {Header, Footer, Nav, Main} from './components/Header';
+
 
 function App() {
   return (
