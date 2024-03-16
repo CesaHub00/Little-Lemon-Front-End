e58e255df6598c8e48705f3f8c5cd0c08b45d2af
CesaHub00 <cesarenava247@gmail.com>
2024-03-16 16:26:38 +0100

    update

---
+++ src/components/Footer.js
@@ -0,0 +1,27 @@
+function Footer() {
+    return (
+      <>
+        <img src="" alt="logo"/>
+        <ul id="doormat-navigation">
+            <li><a href="/home">Home</a></li>
+            <li><a href="/about">About</a></li>
+            <li><a href="/menu">Menu</a></li>
+            <li><a href="/reservation">Reservation</a></li>
+            <li><a href="/order-online">Order Online</a></li>
+            <li><a href="/login">Login</a></li>
+        </ul>
+        <ul id="contact">
+          <li>Address</li>
+          <li>Phone Number</li>
+          <li>email</li>
+        </ul>
+        <ul id="social-media-links">
+          <li><a href="/facebook">Facebook</a></li>
+          <li><a href="/instagram">Instagram</a></li>
+          <li><a href="/twitter">Twitter</a></li>
+        </ul>
+      </>
+    );
+  }
+  
+  export default Footer;
